f4a98540e7d727e02c99d36b64a135fa58ce5ac2
MassimoDanieli <massimo.danieli@openeyes.org.uk>
2015-06-10 14:48:13 +0100

    Update openeyes_installer.sh

--- openeyes_installer.sh
+++ openeyes_installer.sh
@@ -4,20 +4,24 @@ apt-get upgrade -y
 DEBIAN_FRONTEND=noninteractive apt-get install git chef -y
 
 
-
-echo "Usage : openeyes_installer.sh BRANCH_NUMBER"
-        echo ""
-        echo "The available branches are:"
-        echo ""
-                BRANCH=$(git ls-remote --heads  git://github.com/openeyes/OpenEyes.git | awk -F"/" '{print substr($0, index($0, $3))}')
-select ins_bra in $BRANCH;
-do
-        echo "install $ins_bra "
-        break
-done
+if [ $# -lt 1 ]
+then
+        echo "Usage : openeyes_installer.sh BRANCH_NUMBER"
+                echo ""
+                echo "The available branches are:"
+                echo ""
+                        BRANCH=$(git ls-remote --heads  git://github.com/openeyes/OpenEyes.git | awk -F"/" '{print substr($0, index($0, $3))}')
+        select ins_bra in $BRANCH;
+        do
+                echo "install $ins_bra "
+                break
+        done
+else
+        $ins_bra = $1
+fi
 
 echo ""
-echo "I'm now installing OpenEyes branch $ins_bra on $INSTANCE_ID"
+echo "I'm now installing OpenEyes branch $ins_bra"
 
 
 /usr/bin/git clone -b aws --recursive https://github.com/MassimoDanieli/oe_chef.git
